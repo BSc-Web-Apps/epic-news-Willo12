300231c6f7af97e4288bca0b5532e7316b882be2
BrazlyBearWolf <merive33@gmail.com>
2024-11-07 16:23:59 +0000

    added logo to root

--- app/root.tsx
+++ app/root.tsx
@@ -4,23 +4,24 @@ import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
 
 export const links: LinksFunction = () => {
-	return rootLinkElements
+  return rootLinkElements
 }
 export { headers, meta } from './__root.client.tsx'
 export { action, loader } from './__root.server.tsx'
 
 export default function App() {
-	const nonce = useNonce()
+  const nonce = useNonce()
 
-	return (
-		<Document nonce={nonce}>
-			<div className="flex h-screen flex-col justify-between">
-				<div className="flex-1">
-					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
-					</main>
-				</div>
-			</div>
-		</Document>
-	)
+  return (
+    <Document nonce={nonce}>
+      <div className="flex h-screen flex-col justify-between">
+        <div className="flex-1 bg-gray-800">
+          <main className="grid h-full place-items-center">
+            <h1 className="text-mega">bruh</h1>
+            <button className='bg-blue-500 px-6 py-2 rounded-lg text-3xl hover:bg-blue-800 hover:text-4xl transition-all duration-500 border-red-900 '>learn more</button>
+          </main>
+        </div>
+      </div>
+    </Document>
+  )
 }
