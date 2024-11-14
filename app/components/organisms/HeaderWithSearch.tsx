0987e2d9e4ab19b2decf499f5bd8cd7dfbc0e967
BrazlyBearWolf <merive33@gmail.com>
2024-11-14 16:22:56 +0000

    added logo to header and the footer

--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -1,23 +1,26 @@
 import { useMatches } from '@remix-run/react'
 import { SearchBar } from '../molecules/SearchBar'
 import LoginOrUserDropdown from './LoginOrUserDropdown'
-
+import logo from '../../../public/svg/logoTest.svg'
 export default function HeaderWithSearch() {
-	const matches = useMatches()
-	const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
-	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
+  const matches = useMatches()
+  const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
+  const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
 
-	return (
-		<header className="bg-primary/10 py-6 dark:bg-dark-primary/10">
-			<nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
-					{searchBar}
-				</div>
-				<div className="flex items-center gap-10">
-					<LoginOrUserDropdown />
-				</div>
-				<div className="block w-full sm:hidden">{searchBar}</div>
-			</nav>
-		</header>
-	)
+  return (
+    <header className="bg-primary/10 py-6 dark:bg-dark-primary/10">
+      <nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+        <div>
+          <img src={logo} alt="Jersey gaming news logo" className='w-16' />
+        </div>
+        <div className="ml-auto hidden max-w-sm flex-1 sm:block">
+          {searchBar}
+        </div>
+        <div className="flex items-center gap-10">
+          <LoginOrUserDropdown />
+        </div>
+        <div className="block w-full sm:hidden">{searchBar}</div>
+      </nav>
+    </header>
+  )
 }
