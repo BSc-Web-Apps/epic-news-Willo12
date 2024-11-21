8a6cddb7b1ce63ac35d764e70d3c6ec7daf8709c
BrazlyBearWolf <merive33@gmail.com>
2024-11-21 14:15:09 +0000

    added link to home when clicking on the logo

--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -1,4 +1,4 @@
-import { useMatches } from '@remix-run/react'
+import { Link, useMatches } from '@remix-run/react'
 import logo from '../../assets/svg/logoTest.svg'
 import { SearchBar } from '../molecules/SearchBar'
 import LoginOrUserDropdown from './LoginOrUserDropdown'
@@ -10,9 +10,11 @@ export default function HeaderWithSearch() {
   return (
     <header className="bg-primary/10 py-6 dark:bg-dark-primary/10">
       <nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-        <div>
-          <img src={logo} alt="Jersey gaming news logo" className='w-16' />
-        </div>
+        <Link to='/'>
+          <div>
+            <img src={logo} alt="Jersey gaming news logo" className='w-16' />
+          </div>
+        </Link>
         <div className="ml-auto hidden max-w-sm flex-1 sm:block">
           {searchBar}
         </div>
