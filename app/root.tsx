af65902a2197fffff16e5c5265ba89d7632886b8
BrazlyBearWolf <merive33@gmail.com>
2024-11-21 13:53:50 +0000

    made simple headline card

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import HeaderWithSearch from './components/organisms/HeaderWithSearch'
 
 import FooterLogoCentre from './components/organisms/Footer/FooterLogoCentre.tsx'
 import useTheme from './hooks/useTheme.tsx'
+import NewsHeadline from './components/organisms/NewsCard.tsx'
 export const links: LinksFunction = () => {
   return rootLinkElements
 }
@@ -21,12 +22,13 @@ export default function App() {
   const theme = useTheme()
   return (
     <Document nonce={nonce} theme={theme}>
+      <NewsHeadline HeadlineTitle={"hello"} />
       <div className="flex h-screen flex-col justify-between">
         <HeaderWithSearch />
         <div className="flex-1 ">
           <main className="grid h-full place-items-center">
             <h1 className="text-mega">bruh</h1>
-            <button className='bg-blue-500 px-6 py-2 rounded-lg text-3xl hover:bg-blue-800 hover:text-4xl transition-all duration-500 border-red-900 border-8 '>Learn more</button>
+            <button className=''>Learn more</button>
           </main>
         </div>
         <div className='container flex justify-between pb-5'>
