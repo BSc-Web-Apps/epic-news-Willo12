59edbc1166abf29d8620ce509d02eb560de68966
BrazlyBearWolf <merive33@gmail.com>
2024-11-28 16:28:23 +0000

    added comment about links not using css colours properly

--- app/routes/news.tsx
+++ app/routes/news.tsx
@@ -3,31 +3,43 @@ import { NavLink, Outlet } from "@remix-run/react"
 export default function News() {
   return (
     <>
-      <main>
-        <div>
-          <h1>News</h1>
+      <main className="flex flex-col py-24">
+        <div className="container">
+          <h1 className="text-h1">News</h1>
+          <div className="flex space-x-4 py-4">
+            {/* BUG: CSS doesn't work for some reason */}
+            <NavLink
+              to="technology"
+              prefetch="intent"
+              className={({ isActive }) =>
+                `${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500'}`
+              }
+            >
+              Technology
+            </NavLink>
+            <NavLink
+              to="business"
+              prefetch="intent"
+              className={({ isActive }) =>
+                `${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500'}`
+              }
+            >
+              business
+            </NavLink>
+            <NavLink
+              to="yeah"
+              prefetch="intent"
+              className={({ isActive }) =>
+                `${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500'}`
+              }
+            >
+              yeah
+            </NavLink>
+
+          </div>
+          <Outlet />
+
         </div>
-        <div className="flex space-x-4 py-4">
-          <NavLink
-            to="technology"
-            prefetch="intent"
-            className={({ isActive }) =>
-              `${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500'}`
-            }
-          >
-            Technology
-          </NavLink>
-          <NavLink
-            to="business"
-            prefetch="intent"
-            className={({ isActive }) =>
-              `${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500'}`
-            }
-          >
-            business
-          </NavLink>
-        </div>
-        <Outlet />
       </main>
     </>
   )
