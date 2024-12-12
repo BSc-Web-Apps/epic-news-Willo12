d4a4dd8af0d701b6490cbd2921d32d3f88c1d8fe
BrazlyBearWolf <merive33@gmail.com>
2024-12-12 15:06:51 +0000

    prisma database

--- app/components/molecules/MainHeadline.tsx
+++ app/components/molecules/MainHeadline.tsx
@@ -4,11 +4,13 @@ const MainHeadline = () => {
   return (
     <>
       <div className="bg-gray-700 h-80 w-3/5 flex justify-start items-end">
-        <img className="object-fill" src="https://fastly.picsum.photos/id/509/200/300.jpg?hmac=Y2Mtq5PEipyaFNlDH01CoNhW9to1T8GCuTf6yUSH-TY" alt="" />
-        <h1 className="mb-ettext-center w-full px-8 ">
-          MainHeadline text
-        </h1>
-      </div >
+        <div className="bg-gray-800 h-80 w-3/5 flex justify-start items-end">
+          <img className="object-fill" src="https://fastly.picsum.photos/id/509/200/300.jpg?hmac=Y2Mtq5PEipyaFNlDH01CoNhW9to1T8GCuTf6yUSH-TY" alt="" />
+          <h1 className=" w-full px-8 ">
+            MainHeadline text
+          </h1>
+        </div >
+      </div>
     </>
   )
 }
--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -11,11 +11,28 @@ export async function loader({ params }: LoaderFunctionArgs) {
   return json({ categoryTitle })
 }
 
+const WireframeBlock = () => {
+
+  return (
+
+    <div className="h-72 animate-pulse rounded-lg bg-gray-200 dark:bg-gray-700" />
+
+  )
+
+}
+
 export default function NewsCategoryPage() {
   const { categoryTitle } = useLoaderData<typeof loader>()
   return (
     <div className="container py-16">
       <h2 className="text-h2">{categoryTitle}</h2>
+      <div className="grid grid-cols-5 gap-6">
+        <WireframeBlock />
+        <WireframeBlock />
+        <WireframeBlock />
+        <WireframeBlock />
+        <WireframeBlock />
+      </div>
     </div>
   )
 }
