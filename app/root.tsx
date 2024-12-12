7336898c9d49375dddfcf2e3ee0195d5f0e94ce3
BrazlyBearWolf <merive33@gmail.com>
2024-12-12 15:40:31 +0000

    done teh security but I cant sign up

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,13 @@
 import { type LinksFunction } from '@remix-run/node'
 import { Outlet, useLoaderData } from '@remix-run/react'
+import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import Document from '~/components/shared-layout/Document'
 import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
 import { type loader } from './__root.server'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
+import { HoneypotProvider } from 'remix-utils/honeypot/react'
 
 
 import FooterLogoCentre from './components/organisms/Footer/FooterLogoCentre.tsx'
@@ -21,17 +23,21 @@ export default function App() {
   const nonce = useNonce()
   const theme = useTheme()
   return (
-    <Document nonce={nonce} theme={theme}>
-      <div className="flex h-screen flex-col justify-between">
-        <HeaderWithSearch />
-        <div className="flex-1 ">
-          <Outlet />
-        </div>
-        <div className='container flex justify-between pb-5'>
-          <ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
-        </div>
-        <FooterLogoCentre companyName='JerseyGaming' altText='Company Logo' />
-      </div>
-    </Document >
+    <AuthenticityTokenProvider token={data.csrfToken}>
+      <HoneypotProvider {...data.honeyProps}>
+        <Document nonce={nonce} theme={theme}>
+          <div className="flex h-screen flex-col justify-between">
+            <HeaderWithSearch />
+            <div className="flex-1 ">
+              <Outlet />
+            </div>
+            <div className='container flex justify-between pb-5'>
+              <ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+            </div>
+            <FooterLogoCentre companyName='JerseyGaming' altText='Company Logo' />
+          </div>
+        </Document >
+      </HoneypotProvider>
+    </AuthenticityTokenProvider>
   )
 }
