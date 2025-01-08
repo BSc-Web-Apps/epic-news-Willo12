e14f52e47b15638dff9e312d72a9cd3ce9a89bf2
Willo12 <WR114224@hc.ac.je>
2025-01-08 16:21:24 +0000

    added images to articles

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -3,20 +3,23 @@ import { MdBusinessCenter } from "react-icons/md";
 import { LiaTheaterMasksSolid } from 'react-icons/lia'
 import { FaNewspaper } from 'react-icons/fa'
 
+import { getArticleImgSrc } from '~/utils/misc.tsx'
+import siteLogo from '~/assets/svg/icon-placeholder.svg'
 
 
 type Category = "Business" | "Technology" | "Entertainment" | "General News";
 
 
 interface ArticleCardProps {
-  title: String
+  title: string
   category?: Category;
-  imageId: String
+  imageId?: string
 }
 
 
 
 const ArticleCard = ({ title, category = "General News", imageId }: ArticleCardProps) => {
+  const imageSrc = imageId ? getArticleImgSrc(imageId) : siteLogo
   const ArticleIcons: Record<Category, JSX.Element> = {
     Business: <MdBusinessCenter />,
     Technology: <MdComputer />,
@@ -25,11 +28,14 @@ const ArticleCard = ({ title, category = "General News", imageId }: ArticleCardP
   }
   return (
     <>
-      <div className="h-40 bg-red-900 p-4 transition-all duration-500 hover:h-52">
-        <h1 className="font-semibold text-lg">{title}</h1>
-        <div className="flex">
-          {category ? ArticleIcons[category] : <FaNewspaper />}
-          <p>{category}</p>
+      <div>
+        <div><img src={imageSrc} alt={title} /></div>
+        <div className="h-40 bg-red-900 p-4 transition-all duration-500 hover:h-52">
+          <h1 className="font-semibold text-lg">{title}</h1>
+          <div className="flex">
+            {category ? ArticleIcons[category] : <FaNewspaper />}
+            <p>{category}</p>
+          </div>
         </div>
       </div>
     </>
