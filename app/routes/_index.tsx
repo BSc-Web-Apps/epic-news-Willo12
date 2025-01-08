186c60a66acb0fa42dba9ff52a531d361bc100d8
Willo12 <WR114224@hc.ac.je>
2025-01-08 18:30:46 +0000

    added latest news to root

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,72 @@
 import MainHeadline from '#app/components/molecules/MainHeadline.js'
+import ArticleCard from '#app/components/organisms/ArticleCard.js'
 import { type MetaFunction } from '@remix-run/node'
+import { json, Link, useLoaderData } from '@remix-run/react'
+import { prisma } from '~/utils/db.server'
+
 
 export const meta: MetaFunction = () => [{ title: 'Island Gamer' }]
 
+export async function loader() {
+
+  const allArticles = await prisma.article.findMany({
+
+    select: {
+
+      id: true,
+
+      title: true,
+
+      category: { select: { name: true } },
+
+      images: { select: { id: true } },
+
+    },
+
+  })
+
+
+  return json({ allArticles })
+
+}
 export default function Index() {
+  const { allArticles } = useLoaderData<typeof loader>()
   return (
     <>
       <main className="mx-20 mt-5">
-        <div className='flex gap-4'>
-          <MainHeadline />
-          <MainHeadline />
+        <div className="container py-16">
+
+          <h2 className="mb-8 text-h2 font-normal">Latest news</h2>
+
+
+          <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+
+            {allArticles.length > 0 ? (
+
+              allArticles.map(article => (
+
+                <ArticleCard
+
+                  key={article.id}
+
+                  title={article.title}
+
+                  category={article.category?.name}
+
+                  imageId={article.images[0]?.id}
+
+                />
+
+              ))
+
+            ) : (
+
+              <p>No articles found</p>
+
+            )}
+
+          </div>
+
         </div>
       </main>
     </>
