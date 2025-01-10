8e57c0960925eb9b4b2c15cf06d2f7f27978ff9d
BrazlyBearWolf <merive33@gmail.com>
2025-01-10 11:25:59 +0000

    put category name at the bottom of an article card

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -33,11 +33,13 @@ const ArticleCard = ({ title, category = "General News", imageId, articleId }: A
       <Link to={`/article/${articleId}`}>
         <div className="cursor-pointer transition-all duration-500 hover:scale-105">
           <div ><img className="h-64 w-full rounded-t object-cover" src={imageSrc} alt={title} /></div>
-          <div className="h-40 bg-red-900 p-4 transition-all duration-500 hover:h-52">
+          <div className="h-40 bg-red-900 p-4 transition-all duration-500 hover:h-52 flex flex-col justify-between">
             <h1 className="font-semibold text-lg">{title}</h1>
-            <div className="flex">
-              {category ? ArticleIcons[category] : <FaNewspaper />}
-              <p>{category}</p>
+            <div className="flex items-baseline">
+              <div className="flex space-x-1">
+                {category ? ArticleIcons[category] : <FaNewspaper />}
+                <p>{category}</p>
+              </div>
             </div>
           </div>
         </div>
